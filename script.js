02e61bba49d4ea03d7c8403545f02e5f029dcfd6
murdakCreations <96468303+murdakCreations@users.noreply.github.com>
2026-04-24 14:51:59 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Dummy SMDC Landing Page Loaded");
+
+document.querySelector('.cta-button').addEventListener('click', function () {
+    alert("Thank you for exploring our projects!");
+});
